9761d1130f09e08239a9419c4c87b47b8fe855f5
muhammed tarık mengüç <108359345+tarikmenguc@users.noreply.github.com>
2024-10-24 23:15:32 +0300

    Basic Flappy Ball

---
+++ Flappy Ball/Flappy Ball/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Flappy_Ball
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.scoree = new System.Windows.Forms.Label();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.BotPipe = new System.Windows.Forms.PictureBox();
+            this.topPipe = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BotPipe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topPipe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // scoree
+            // 
+            this.scoree.AutoSize = true;
+            this.scoree.Font = new System.Drawing.Font("Georgia", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoree.Location = new System.Drawing.Point(12, 9);
+            this.scoree.Name = "scoree";
+            this.scoree.Size = new System.Drawing.Size(104, 24);
+            this.scoree.TabIndex = 1;
+            this.scoree.Text = "SCORE:0";
+            // 
+            // Ball
+            // 
+            this.Ball.Image = global::Flappy_Ball.Properties.Resources.icons8_flappy_dunk_100_removebg_preview;
+            this.Ball.Location = new System.Drawing.Point(488, 276);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(100, 100);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // BotPipe
+            // 
+            this.BotPipe.Image = global::Flappy_Ball.Properties.Resources.pipes__1__removebg_preview__1_;
+            this.BotPipe.Location = new System.Drawing.Point(842, 444);
+            this.BotPipe.Name = "BotPipe";
+            this.BotPipe.Size = new System.Drawing.Size(128, 135);
+            this.BotPipe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BotPipe.TabIndex = 0;
+            this.BotPipe.TabStop = false;
+            // 
+            // topPipe
+            // 
+            this.topPipe.Image = global::Flappy_Ball.Properties.Resources.pipes_removebg_preview;
+            this.topPipe.Location = new System.Drawing.Point(853, 0);
+            this.topPipe.Name = "topPipe";
+            this.topPipe.Size = new System.Drawing.Size(128, 185);
+            this.topPipe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.topPipe.TabIndex = 0;
+            this.topPipe.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ground.Image = ((System.Drawing.Image)(resources.GetObject("ground.Image")));
+            this.ground.Location = new System.Drawing.Point(0, 575);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(1132, 61);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 2;
+            this.ground.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1132, 636);
+            this.Controls.Add(this.scoree);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.BotPipe);
+            this.Controls.Add(this.topPipe);
+            this.Controls.Add(this.ground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BotPipe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.topPipe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox topPipe;
+        private System.Windows.Forms.PictureBox BotPipe;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label scoree;
+        private System.Windows.Forms.PictureBox ground;
+    }
+}
+
---
+++ Flappy Ball/Flappy Ball/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flappy_Ball
+{
+    public partial class Form1 : Form
+    {
+
+        int pipeSpeed = 6; // default pipe speed defined with an integer
+        int gravity = 4; // default gravity speed defined with an integer
+        int score = 0; // default score integer set to 0
+        // variable ends
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KHandleKeyDownyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.HandleKeyUp);
+
+        }
+        private void KHandleKeyDownyDown(object sender, KeyEventArgs e)
+        {
+            // this is the game key is down event thats linked to the main form
+            if (e.KeyCode == Keys.Space)
+            {
+                // if the space key is pressed then the gravity will be set to -15
+                gravity = -15;
+            }
+
+
+        }
+
+        private void HandleKeyUp(object sender, KeyEventArgs e)
+        {
+            // this is the game key is up event thats linked to the main form
+
+            if (e.KeyCode == Keys.Space)
+            {
+                // if the space key is released then gravity is set back to 15
+                gravity = 15;
+            }
+
+        }
+
+        private void endGame()
+        {
+            // this is the game end function, this function will when the bird touches the ground or the pipes
+            GameTimer.Stop(); // stop the main timer
+            scoree.Text += " Game over!!!"; // show the game over text on the score text, += is used to add the new string of text next to the score instead of overriding it
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            Ball.Top += gravity; // link the flappy bird picture box to the gravity, += means it will add the speed of gravity to the picture boxes top location so it will move down
+            BotPipe.Left -= pipeSpeed; // link the bottom pipes left position to the pipe speed integer, it will reduce the pipe speed value from the left position of the pipe picture box so it will move left with each tick
+            topPipe.Left -= pipeSpeed; // the same is happening with the top pipe, reduce the value of pipe speed integer from the left position of the pipe using the -= sign
+            scoree.Text = "Score: " + score; // show the current score on the score text label
+
+            // below we are checking if any of the pipes have left the screen
+
+            if (BotPipe.Left < -150)
+            {
+                // if the bottom pipes location is -150 then we will reset it back to 800 and add 1 to the score
+                BotPipe.Left = 800;
+                score++;
+            }
+            if (topPipe.Left < -180)
+            {
+                // if the top pipe location is -180 then we will reset the pipe back to the 950 and add 1 to the score
+                topPipe.Left = 950;
+                score++;
+            }
+
+            // the if statement below is checking if the pipe hit the ground, pipes or if the player has left the screen from the top
+            // the two pipe symbols stand for OR inside of an if statement so we can have multiple conditions inside of this if statement because its all going to do the same thing
+
+            if (Ball.Bounds.IntersectsWith(BotPipe.Bounds) ||
+                Ball.Bounds.IntersectsWith(topPipe.Bounds) ||
+                Ball.Bounds.IntersectsWith(ground.Bounds) || Ball.Top < -25
+                )
+            {
+                // if any of the conditions are met from above then we will run the end game function
+                endGame();
+            }
+
+
+            // if score is greater then 5 then we will increase the pipe speed to 15
+            if (score > 5)
+            {
+                pipeSpeed = 15;
+            }
+
+        }
+
+    }
+}
+
